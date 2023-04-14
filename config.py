6a777d556449d119a748e1d8a77e38f1f551bd6f
Viet Nguyen <vietjovi@gmail.com>
2023-04-14 16:12:59 +0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+api_key = ''
+api_endpoint = 'https://xxxxxxxx.openai.azure.com/' #Remove this configuration if you use *.openai.com
+api_version = "2023-03-15-preview"
+api_type = "azure" #Remove this configuration if you use *.openai.com
+engine = "gpt-3.5-turbo"
+version = "v1.0"
+data_dir = './data/'
+past_message_included = "5" #How many message history(Q&A) will be kept. It makes a price increase - https://openai.com/pricing
